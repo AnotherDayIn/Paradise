13ced6bb2bbaf93b66b9d0a1117c45eaed097f7f
rb <redblue1776@icloud.com>
2021-05-28 03:57:53 -0500

    Projects + and Linq provider

---
+++ src/SphereDBFileAdapter/Place.cs
@@ -0,0 +1,20 @@
+﻿namespace Linq2SphereDBProvider
+{
+    public class Place
+    {
+        // Properties. 
+        public string Name { get; private set; }
+        public string State { get; private set; }
+        public PlaceType PlaceType { get; private set; }
+
+        // Constructor. 
+        internal Place(string name,
+                        string state,
+                      SphereDBQueryProvider.TerraServerReference.PlaceType placeType)
+        {
+            Name = name;
+            State = state;
+            PlaceType = (PlaceType)placeType;
+        }
+    }
+}
---
+++ src/SphereDBFileAdapter/PlaceType.cs
@@ -0,0 +1,19 @@
+﻿namespace Linq2SphereDBProvider
+{
+    public enum PlaceType
+    {
+        Unknown,
+        AirRailStation,
+        BayGulf,
+        CapePeninsula,
+        CityTown,
+        HillMountain,
+        Island,
+        Lake,
+        OtherLandFeature,
+        OtherWaterFeature,
+        ParkBeach,
+        PointOfInterest,
+        River
+    }
+}
