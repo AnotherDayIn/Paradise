631d5551f149be17e3748f1e341e2465946835a7
rb <redblue1776@icloud.com>
2021-05-28 04:39:16 -0500

    desktop support

---
+++ src/Paradise/Paradise.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paradise.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
